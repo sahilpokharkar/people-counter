db17ae5575d2ef412f9e1ec8c80ec0f6246a31c3
CyberSam-11 <sahilpokharkar3@gmail.com>
2023-08-20 17:26:40 +0530

    Updated

---
+++ index.js
@@ -0,0 +1,18 @@
+
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+
+function increment() {
+    
+    count += 1
+    countEl.innerHTML = count
+}
+
+function save() {
+
+    saveEl.innerHTML += count + " - "
+    count = 0
+    countEl.innerHTML = count
+}
